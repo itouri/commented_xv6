db22e43fa13823ca90c3ee9ef0b53b190d0ae94b
akiyama_kousuke <akiyama_kousuke@192-168-22-176.i.sslab.ics.keio.ac.jp>
2016-05-26 15:31:53 +0900

    no message

--- proc.h
+++ proc.h
@@ -56,7 +56,7 @@ enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 struct proc {
   //ユーザースタックのアドレス空間は指定しないの? 
   uint sz;                     // Size of process memory (bytes)
-  pde_t* pgdir;                // Page table ページングの機構がつかうらしい
+  pde_t* pgdir;                // Page tableのアドレス ページングの機構がつかうらしい pde_t = uint
   char *kstack;                // Bottom of kernel stack for this process
   enum procstate state;        // Process state
   int pid;                     // Process ID
